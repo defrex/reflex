98ae5eb9f6f62899bfced2f633755bc447cad925
Aron Jones <aron.jones@gmail.com>
2019-03-05 19:54:29 -0500

    remove comments

--- api/github/index.ts
+++ api/github/index.ts
@@ -4,9 +4,4 @@ export default (app: Application) => {
   app.on('pull_request', async (context) => {
     app.log(context)
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
