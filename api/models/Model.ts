6d048922c48e03fea836a4d9867c32f9f869bc8d
Aron Jones <aron.jones@gmail.com>
2019-03-08 22:09:46 -0500

    add graph id resolution methods to Model

--- api/models/Model.ts
+++ api/models/Model.ts
@@ -1,9 +1,22 @@
 import { BaseEntity, PrimaryGeneratedColumn } from 'typeorm'
 
-// import 'api/db' // ensure this is loaded if a Model is loaded
-// console.log('connection')
+import { graphIdforModel, modelForGraphId } from 'api/lib/graphIds'
+
+export type StaticThis<T> = { new (): T }
 
 export default abstract class Model extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number
+
+  public async fromGraphId<T extends Model> (this: StaticThis<T>, graphId: string): Promise<T> {
+    const model = await modelForGraphId(graphId)
+    if (model instanceof this) {
+      return model
+    }
+    throw new Error(`Expected ${this}, got ${model}`)
+  }
+
+  async graphId () {
+    return graphIdforModel(this)
+  }
 }
