747971be51691b8a47cf37e7c306c3305e48f638
Aron Jones <aron.jones@gmail.com>
2019-03-10 11:34:00 -0400

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withTypescript = require('@zeit/next-typescript')
 const path = require('path')
 
-const config = require('./config').default
-
 module.exports = withTypescript({
   useFileSystemPublicRoutes: false,
 
   webpack: (webpackConfig) => {
-    webpackConfig.resolve.alias.ui = path.join(config.basePath, 'ui')
-    webpackConfig.resolve.alias.gen = path.join(config.basePath, 'gen')
+    webpackConfig.resolve.alias.ui = path.join(__dirname, 'ui')
+    webpackConfig.resolve.alias.gen = path.join(__dirname, 'gen')
 
     webpackConfig.module.rules.push({
       test: /\.graphql$/,
