9c95c82395c9bd521cd7bf4837b5fd15cb019709
Aron Jones <aron.jones@gmail.com>
2019-03-09 19:41:26 -0500

    small rev

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export default class IndexPage extends React.PureComponent {
   render () {
     return (
-      <div>Reflex UI t4</div>
+      <div>Reflex UI t5</div>
     )
   }
 }
