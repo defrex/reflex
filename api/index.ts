2ad7a7fc87ced09a54b850cc50c279bc083a118a
Aron Jones <aron.jones@gmail.com>
2019-03-10 14:35:12 -0400

    context type

--- api/index.ts
+++ api/index.ts
@@ -2,11 +2,10 @@ import { GraphQLServer } from 'graphql-yoga'
 
 import resolvers from 'api/resolvers'
 import config from 'api/config'
+import { getContext } from 'api/context'
 
 export default new GraphQLServer({
   typeDefs: config.graphqlSchemaPath,
   resolvers,
-  context: async (_req, _res) => {
-    return {}
-  },
+  context: getContext,
 })
