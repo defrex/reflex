fdf9b93cf5193f68a7c073e105dbd6f042558d8f
Aron Jones <aron.jones@gmail.com>
2019-03-14 17:20:07 -0400

    add a logo to the homepage

--- ui/pages/index/index.tsx
+++ ui/pages/index/index.tsx
@@ -3,7 +3,30 @@ import * as React from 'react'
 export default class IndexPage extends React.PureComponent {
   render () {
     return (
-      <div>Reflex UI t4</div>
+      <div className='page'>
+        <img src='/static/protologo.svg' alt='Reflex'/>
+        <style jsx global>{`
+          html, body {
+            margin: 0;
+            min-height: 100vh;
+            width: 100vw;
+          }
+        `}</style>
+        <style jsx>{`
+          .page {
+            min-height: 100vh;
+            width: 100vw;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #0a1c35;
+          }
+
+          img {
+            max-height: 100px;
+          }
+        `}</style>
+      </div>
     )
   }
 }
