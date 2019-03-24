5b38ff77db1c99d790345c4398a0a8951e9b369f
Aron Jones <aron.jones@gmail.com>
2019-03-24 13:57:55 -0400

    create org mutation wip

--- ui/pages/createOrganization/index.tsx
+++ ui/pages/createOrganization/index.tsx
@@ -1,15 +1,39 @@
 import React, { PureComponent } from 'react'
 
+import routes from 'ui/routes'
 import Page from 'ui/components/Page'
 import CreateOrganizationForm from 'ui/components/CreateOrganizationForm'
 
 import {
   OrganizationsPageQueryComponent,
   CreateOrganizationPageMutationComponent,
+  CreateOrganizationPageMutationMutation,
+  CreateOrganizationPageMutationMutationVariables,
 } from 'ui/lib/graphql'
 import styles from './styles'
+import { MutationFn } from 'react-apollo'
 
 export default class OrganizationsPage extends PureComponent {
+  handleSubmit = (
+    mutate: MutationFn<
+      CreateOrganizationPageMutationMutation,
+      CreateOrganizationPageMutationMutationVariables
+    >,
+  ) => async (values: CreateOrganizationPageMutationMutationVariables) => {
+    const response = await mutate({ variables: values })
+    if (
+      response &&
+      response.data &&
+      response.data.createOrganization &&
+      response.data.createOrganization.status.success
+    ) {
+      routes.pushRoute('organization', {
+        id: response.data.createOrganization.organization!.id,
+      })
+    } else {
+      console.error('💢', response)
+    }
+  }
   render() {
     return (
       <OrganizationsPageQueryComponent>
@@ -20,7 +44,7 @@ export default class OrganizationsPage extends PureComponent {
               <CreateOrganizationPageMutationComponent>
                 {(createOrganization) => (
                   <CreateOrganizationForm
-                    onSubmit={(variables) => createOrganization({ variables })}
+                    onSubmit={this.handleSubmit(createOrganization)}
                   />
                 )}
               </CreateOrganizationPageMutationComponent>
