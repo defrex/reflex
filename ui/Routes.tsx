cc7e966080ef75fa62652fdcb40036ffb6619ebc
Aron Jones <aron.jones@gmail.com>
2019-03-31 11:40:25 -0400

    rename pages

--- ui/Routes.tsx
+++ ui/Routes.tsx
@@ -1,11 +1,11 @@
 import React, { PureComponent } from 'react'
 import { Route } from 'react-router-dom'
 
-import Index from 'ui/pages/index'
-import Dashboard from 'ui/pages/dashboard'
-import Library from 'ui/pages/library'
-import Team from 'ui/pages/team'
-import CreateTeam from 'ui/pages/createTeam'
+import Index from 'ui/pages/Index'
+import Dashboard from 'ui/pages/Dashboard'
+import Library from 'ui/pages/Library'
+import Team from 'ui/pages/Team'
+import CreateTeam from 'ui/pages/CreateTeam'
 
 export default class Router extends PureComponent {
   render() {
