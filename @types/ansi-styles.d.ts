2dffb1ca0679da912d1cc614aa33c9b80554ce9e
Aron Jones <aron.jones@gmail.com>
2019-04-03 18:11:09 -0400

    remove bad colors

---
+++ @types/ansi-styles.d.ts
@@ -0,0 +1,46 @@
+declare module 'ansi-styles' {
+  interface Style {
+    open: string
+    close: string
+  }
+  export const reset: Style
+  export const bold: Style
+  export const dim: Style
+  export const italic: Style
+  export const underline: Style
+  export const inverse: Style
+  export const hidden: Style
+  export const strikethrough: Style
+  export const black: Style
+  export const red: Style
+  export const green: Style
+  export const yellow: Style
+  export const blue: Style
+  export const magenta: Style
+  export const cyan: Style
+  export const white: Style
+  export const grey: Style
+  export const redBright: Style
+  export const greenBright: Style
+  export const yellowBright: Style
+  export const blueBright: Style
+  export const magentaBright: Style
+  export const cyanBright: Style
+  export const whiteBright: Style
+  export const bgBlack: Style
+  export const bgRed: Style
+  export const bgGreen: Style
+  export const bgYellow: Style
+  export const bgBlue: Style
+  export const bgMagenta: Style
+  export const bgCyan: Style
+  export const bgWhite: Style
+  export const bgGrey: Style
+  export const bgRedBright: Style
+  export const bgGreenBright: Style
+  export const bgYellowBright: Style
+  export const bgBlueBright: Style
+  export const bgMagentaBright: Style
+  export const bgCyanBright: Style
+  export const bgWhiteBright: Style
+}
--- bin/start.js
+++ bin/start.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
 require('./lib/setup')
+require('./lib/recolor')
+
 require('../index.ts').default()
