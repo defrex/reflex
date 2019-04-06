b5df2b15bba4e9ff35384c7746ef123b0e9981f4
Aron Jones <aron.jones@gmail.com>
2019-04-06 11:09:27 -0400

    test rendering

---
+++ api/lib/render/test.ts
@@ -0,0 +1,18 @@
+import fs from 'fs'
+
+import { absolutePath } from 'api/lib/path'
+import { renderUrl } from 'api/lib/render'
+import { promisify } from 'util'
+
+const readFile = promisify(fs.readFile)
+
+describe('renderUrl', () => {
+  it('renders', async () => {
+    const result = await renderUrl('https://defrex.com')
+    const prevResult = await readFile(
+      absolutePath('api/lib/render/test.result.png'),
+    )
+
+    expect(result).toEqual(prevResult)
+  })
+})
