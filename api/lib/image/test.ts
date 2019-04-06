830909d808a092e1d7c67079056641ab4e45c4c7
Aron Jones <aron.jones@gmail.com>
2019-04-06 11:37:02 -0400

    remove the renders dir from git

--- api/lib/image/test.ts
+++ api/lib/image/test.ts
@@ -5,10 +5,13 @@ import { trimImage } from 'api/lib/image'
 import { absolutePath } from 'api/lib/path'
 
 const readFile = promisify(fs.readFile)
+// const writeFile = promisify(fs.writeFile)
 
 describe('trimImage', () => {
   it('trims', async () => {
     const result = await trimImage(absolutePath('api/lib/image/test.png'))
+    // await writeFile(absolutePath('api/lib/image/test.result.png'), result)
+
     const expectedResult = await readFile(
       absolutePath('api/lib/image/test.result.png'),
     )
--- api/lib/render/test.ts
+++ api/lib/render/test.ts
@@ -5,10 +5,13 @@ import { renderUrl } from 'api/lib/render'
 import { promisify } from 'util'
 
 const readFile = promisify(fs.readFile)
+// const writeFile = promisify(fs.writeFile)
 
 describe('renderUrl', () => {
   it('renders', async () => {
     const result = await renderUrl('https://defrex.com')
+    // await writeFile(absolutePath('api/lib/render/test.result.png'), result)
+
     const expectedResult = await readFile(
       absolutePath('api/lib/render/test.result.png'),
     )
