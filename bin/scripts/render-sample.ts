678a4c2c0f042ec0097b27317675f2fc9709fc98
Aron Jones <aron.jones@gmail.com>
2019-04-07 13:05:38 -0400

    try rendering a component

--- bin/scripts/render-sample.ts
+++ bin/scripts/render-sample.ts
@@ -3,11 +3,16 @@ import { Storage } from '@google-cloud/storage'
 import { trimImage } from 'api/lib/image'
 import { renderUrl } from 'api/lib/render'
 import config from 'api/config'
+import { absoluteUrl } from 'ui/lib/url'
 
 const storage = new Storage({ projectId: config.gcpProjectId })
 const bucketName = 'reflexui.com'
 const directoryName = `renders/${config.environment}`
 const time = new Date().valueOf()
+
+// const teamId = 'cjtp6ia6s00as0748aq40bbwq' // dev
+const teamId = 'cjtpxdt9z00690804knk2emtz' // prod
+const url = absoluteUrl(`/teams/${teamId}/examples/Button/Primary`)
 
 async function upload(filename: string, content: Buffer) {
   return new Promise((resolve, reject) => {
@@ -22,9 +27,9 @@ async function upload(filename: string, content: Buffer) {
 }
 
 export default async function main() {
-  let image = await renderUrl('https://www.google.com/')
+  let image = await renderUrl(url)
   try {
-    await upload(`${directoryName}/browser-${time}.png`, image)
+    await upload(`${directoryName}/${time}-browser.png`, image)
   } catch (error) {
     console.error(error)
     return
@@ -32,7 +37,7 @@ export default async function main() {
 
   image = await trimImage(image)
   try {
-    await upload(`${directoryName}/trim-${time}.png`, image)
+    await upload(`${directoryName}/${time}-trim.png`, image)
   } catch (error) {
     console.error(error)
     return
