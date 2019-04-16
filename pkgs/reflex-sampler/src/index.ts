82af7c42794f19a07f57c7b2142bb859ce7d763a
Aron Jones <aron.jones@gmail.com>
2019-04-16 18:03:00 -0400

    ack message

--- pkgs/reflex-sampler/src/index.ts
+++ pkgs/reflex-sampler/src/index.ts
@@ -1,17 +1,14 @@
 import { Message } from '@google-cloud/pubsub'
 
-interface Event {
-  eventId: string
-  resource: any
-}
-
 interface Payload {
   hello: string
 }
 
-export function main(message: Message, event: Event) {
+export function main(message: Message) {
   const payload: Payload = JSON.parse(
     Buffer.from((message.data as unknown) as string, 'base64').toString(),
   )
   console.log(`Hello ${payload.hello}`)
+
+  message.ack()
 }
