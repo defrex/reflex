d6423d3e9094f6486979eb8a3905cd37a2d8dc8b
Aron Jones <aron.jones@gmail.com>
2019-04-16 16:59:31 -0400

    use the correct URI

--- pkgs/reflex-sampler/bin/main/deploy.ts
+++ pkgs/reflex-sampler/bin/main/deploy.ts
@@ -2,13 +2,13 @@ import run from '../lib/run'
 import uploadPackage from '../lib/uploadPackage'
 
 export async function main() {
-  await uploadPackage()
+  const uri = await uploadPackage()
   await run([
     'gcloud',
     'functions',
     'deploy',
     'sampler',
     '--project=reflex-236915',
-    '--source=gs://reflex-sampler/builds/1555379273973.zip',
+    `--source=${uri}`,
   ])
 }
