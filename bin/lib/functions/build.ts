197b388e9cf2ef9a1223d1aa44acea2113867c52
Aron Jones <aron.jones@gmail.com>
2019-04-28 20:34:16 -0400

    use npm for function build

--- bin/lib/functions/build.ts
+++ bin/lib/functions/build.ts
@@ -2,5 +2,5 @@ import absolutePath from 'bin/lib/absolutePath'
 import run from 'bin/lib/run'
 
 export default async function main(functionName: string) {
-  await run(['yarn', 'build'], absolutePath(`functions/${functionName}`))
+  await run(['npm', 'run', 'build'], absolutePath(`functions/${functionName}`))
 }
