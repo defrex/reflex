afa48371f8f4f4b97a6e19f420fbde56f61d1931
Aron Jones <aron.jones@gmail.com>
2019-05-04 12:46:54 -0400

    print the key to confirm format

--- services/api/github/webhooks/index.ts
+++ services/api/github/webhooks/index.ts
@@ -1,8 +1,8 @@
-import { Probot, Application } from 'probot'
-
 import config from 'api/config'
 import checkSuite from 'api/github/webhooks/checkSuite'
+import { Application, Probot } from 'probot'
 
+console.log(config.githubAppPrivateKey)
 const probot = new Probot({
   id: config.githubAppId,
   secret: config.githubAppWebhookSecret,
