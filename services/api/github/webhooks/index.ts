04cbb2207962df077d6c4b2c4de9373c5705b2a1
Aron Jones <aron.jones@gmail.com>
2019-05-04 12:56:24 -0400

    more cert format debugging

--- services/api/github/webhooks/index.ts
+++ services/api/github/webhooks/index.ts
@@ -2,11 +2,23 @@ import config from 'api/config'
 import checkSuite from 'api/github/webhooks/checkSuite'
 import { Application, Probot } from 'probot'
 
-console.log(config.githubAppPrivateKey)
+console.log('githubAppPrivateKey', config.githubAppPrivateKey)
+console.log(
+  'githubAppPrivateKey.newlines',
+  (config.githubAppPrivateKey.match(/\n/g) || []).length,
+)
+const cert = config.githubAppPrivateKey
+  .replace('-----BEGIN RSA PRIVATE KEY-----', '')
+  .replace('-----END RSA PRIVATE KEY-----', '')
+  .trim()
+  .split(' ')
+  .join('\n')
+console.log('cert', cert)
+
 const probot = new Probot({
   id: config.githubAppId,
   secret: config.githubAppWebhookSecret,
-  cert: config.githubAppPrivateKey,
+  cert, //: config.githubAppPrivateKey,
   // tunnel: process.env.SUBDOMAIN || process.env.NODE_ENV !== 'production',
 })
 
